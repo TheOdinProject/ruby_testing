03849d09ebb92c8b4e414d4dd57101b4f597c003
Semaphore <kristen.m.eversole@gmail.com>
2022-07-09 19:23:26 -0500

    Fixed typo: String Spec

--- spec/01_string_spec.rb
+++ spec/01_string_spec.rb
@@ -28,7 +28,7 @@ describe String do
   end
 
   # Let variables reset between examples.
-  context 'when the overriden value is out of scope' do
+  context 'when the overridden value is out of scope' do
     it 'is the value of original let variable' do
       expect(favorite_color).to eq('blue')
     end
