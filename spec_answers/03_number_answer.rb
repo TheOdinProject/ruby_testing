5482cc305f07034d60a756e9dc4d60373c5dd01d
Lorenzo <97030518+Uaitt@users.noreply.github.com>
2022-09-20 15:20:12 +0200

    ./spec_answers/03_number_answer.rb: Fix subject declaration
    Fix variable declaration not matching the comment

--- spec_answers/03_number_answer.rb
+++ spec_answers/03_number_answer.rb
@@ -3,7 +3,7 @@
 describe Array do
   context 'when my_array has perfect values' do
     # Write a subject variable 'my_array' that passes all tests.
-    let(:my_array) { [-7, 42, 101, 98] }
+    subject(:my_array) { [-7, 42, 101, 98] }
 
     # remove the 'x' before running this test
     it 'has a specific first value' do
